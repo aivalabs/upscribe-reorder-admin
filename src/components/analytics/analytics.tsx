861187b48bf94ec2ad251b380f062e4ac065ffdd
fazlulaiva <fazlul.kabir@aivalabs.com>
2022-07-27 22:23:34 +0600

    fixed: undefined value issue

--- src/components/analytics/analytics.tsx
+++ src/components/analytics/analytics.tsx
@@ -8,39 +8,45 @@ import { IOrderCountSources, ITopReorderedProducts } from "./types";
 
 export default function Analytics() {
    const {
-      orderCountSources: {orderCounts, revenue, other}, 
-      setIOrderCountSources, 
-      topReorderedProducts, 
+      orderCountSources: { orderCounts, revenue, other },
+      setIOrderCountSources,
+      topReorderedProducts,
       setTopReorderedProducts } = useAnalytics();
-   
+
    const { isLoading, setIsLoading } = useUtility();
-   
-   
+
+
    useEffect((): any => {
       let mounted = true;
-      if(!isLoading && mounted) setIsLoading(true);
+      if (!isLoading && mounted) setIsLoading(true);
       orderCounters()
-      .then((data: IOrderCountSources) => {
-         if(mounted) setIOrderCountSources(data);
-         if(isLoading) setIsLoading(false);
-      })
-      .catch((err: Error) => console.log(err));
+         .then((data: IOrderCountSources) => {
+            if (mounted) setIOrderCountSources(data);
+            if (isLoading) setIsLoading(false);
+         })
+         .catch((err: Error) => console.log(err));
       getTopReorderedProducts();
       return () => mounted = false;
-   // eslint-disable-next-line react-hooks/exhaustive-deps
+      // eslint-disable-next-line react-hooks/exhaustive-deps
    }, []);
    const getTopReorderedProducts = (): void => {
       topReorderedProductsAPI()
-      .then((data: ITopReorderedProducts[]) => {
-         setTopReorderedProducts(data);
-      })
-      .catch((err: Error) => console.log(err));
+         .then((data: ITopReorderedProducts[]) => {
+            setTopReorderedProducts(data);
+         })
+         .catch((err: Error) => console.log(err));
    }
-  return (
+   return (
       <>
-         <CardSection title="Number of Orders" card={orderCounts}/>
-         <CardSection title="Total Revenue Generated" card={revenue}/>
-         <Other title="Others" other={other} topReorderedProducts={topReorderedProducts} />
+         {
+            isLoading ?
+               <></>
+               : <>
+                  <CardSection title="Number of Orders" card={orderCounts} />
+                  <CardSection title="Total Revenue Generated" card={revenue} />
+                  <Other title="Others" other={other} topReorderedProducts={topReorderedProducts} />
+               </>
+         }
       </>
-  );
+   );
 }
